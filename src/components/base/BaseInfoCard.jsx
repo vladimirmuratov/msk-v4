f154140740404dca34da8dfc98792b4f5557963e
vladimir <vladimirmuratov@mail.ru>
2024-12-03 12:30:38 +0300

    add alt text

--- src/components/base/BaseInfoCard.jsx
+++ src/components/base/BaseInfoCard.jsx
@@ -1,13 +1,6 @@
 import {Box, Typography} from '@mui/material'
 
-export const BaseInfoCard = ({id, img, count, title, desc}) => {
-    let altText = id === 1
-        ? 'платная неврология москва'
-        : id === 2
-            ? ' больница терапия платная палата'
-            : id === 3
-                ? 'платная госпитализация урология'
-                : ' платные палаты кардиологии'
+export const BaseInfoCard = ({alt, img, count, title, desc}) => {
     return (
         <Box
             sx={{
@@ -17,7 +10,7 @@ export const BaseInfoCard = ({id, img, count, title, desc}) => {
                 gap: '10px',
                 color: 'var(--white)',
             }}>
-            <img src={img} alt={altText} style={{width: '100px', height: '100px'}}/>
+            <img src={img} alt={alt} style={{width: '100px', height: '100px'}}/>
             <Typography sx={{fontSize: 35}}>{count}</Typography>
             <Typography sx={{fontWeight: 700, textTransform: 'uppercase', textAlign: 'center'}}>{title}</Typography>
             <Typography sx={{
